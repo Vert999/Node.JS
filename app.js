ec1bb27f5fc853d503cd584e8f551b0237462ccb
SANDERS <as0952521@otc.edu>
2022-11-29 08:55:14 -0600

    update commit

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+//Git init
+//Git Add *
+//git commit -m "any message"
+// git remote add origin (link to your github repository)
+// git remote -v (to verify)
+// git push -u origin master
+// if you add another file or update, just git add *, git init, and git push
+
 console.log("Putting the init in Git");
